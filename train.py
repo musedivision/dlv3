8dca07fe3e40e9a1685659ec57721114e5d7508c
patricio <patrick.mccaffrey.19@gmail.com>
2019-05-04 22:09:06 +1200

    😅 starting  agin

--- train.py
+++ train.py
@@ -6,20 +6,25 @@ from fastai.vision import ImageDataBunch, imagenet_stats, get_transforms
 import numpy as np
 import torch
 
-def main():
-    path = untar_data(URLs.PETS)
-    path_anno = path/'annotations'
-    path_images = path/'images'
-    fnames = get_image_files(path_images)
+class catsdogs {
+    def __init__():
+              
+    
+    def createDataLoader(self):
+        path = untar_data(URLs.PETS)
+        path_anno = path/'annotations'
+        path_images = path/'images'
+        fnames = get_image_files(path_images)
 
-    np.random.seed(2)
-    pat = re.compile(r'/([^/]+)_\d+.jpg$')
-   
-    bs = 6
-    # create data loaderi
-    data =ImageDataBunch.from_name_re(path, fnames, pat, ds_tfms=get_transforms(), size=224, bs=bs).normalize(imagenet_stats)
-    learn = create_cnn(data, models.resnet34, metrics=error_rate)
-    learn.fit_one_cycle(4)
+        np.random.seed(2)
+        pat = re.compile(r'/([^/]+)_\d+.jpg$')
+       
+        bs = 6
+        # create data loaderi
+        self.data =ImageDataBunch.from_name_re(path, fnames, pat, ds_tfms=get_transforms(), size=224, bs=bs).normalize(imagenet_stats)
+    
+    def getModel(self):
+         self.learner = create_cnn(dataloader, models.resnet34, metrics=error_rate)
 
 
 if __name__ == '__main__':
